14d2673d48e14c6163bdbcf803e995187b667e4c
David Matejka <matej21@matej21.cz>
2018-03-14 17:59:43 +0100

    init

---
+++ src/Bridges/Infrastructure/MocksContainerHook.php
@@ -0,0 +1,40 @@
+<?php declare(strict_types = 1);
+
+namespace Mangoweb\Tester\HttpMocks\Bridges\Infrastructure;
+
+use Mangoweb\Tester\HttpMocks\HttpRequest;
+use Mangoweb\Tester\HttpMocks\Session;
+use Mangoweb\Tester\Infrastructure\Container\AppContainerHook;
+use Nette\DI\ContainerBuilder;
+use Nette\DI\Statement;
+use Nette\Http\Request;
+use Nette\Http\UrlScript;
+
+
+class MocksContainerHook extends AppContainerHook
+{
+	/** @var string */
+	private $baseUrl;
+
+
+	public function __construct(string $baseUrl)
+	{
+		$this->baseUrl = $baseUrl;
+	}
+
+
+	public function onCompile(ContainerBuilder $builder): void
+	{
+		if ($builder->hasDefinition('http.request')) {
+			$builder->getDefinition('http.request')
+				->setClass(Request::class)
+				->setFactory(HttpRequest::class, [new Statement(UrlScript::class, [$this->baseUrl])]);
+		}
+
+		if ($builder->hasDefinition('session.session')) {
+			$builder->getDefinition('session.session')
+				->setClass(\Nette\Http\Session::class)
+				->setFactory(Session::class);
+		}
+	}
+}
